4bf46e5c418a19f7efe352133b3d44d45c3521fe
zPirroZ3007 <giusepirro@hotmail.com>
2016-12-28 18:16:20 +0100

    Update FixBungee.java

--- me/zpirroz/uuidfix/FixBungee.java
+++ me/zpirroz/uuidfix/FixBungee.java
@@ -7,20 +7,53 @@
  */
 package me.zpirroz.uuidfix;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.UUID;
+
+import com.google.common.base.Charsets;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
 public class FixBungee extends Plugin implements Listener
 {
+	@SuppressWarnings("unused")
 	@Override
 	public void onEnable()
 	{
+		if (!getDataFolder().exists())
+		{
+			getDataFolder().mkdir();
+		}
+		File configFile = new File(getDataFolder(), "config.yml");
+		if (!getDataFolder().exists())
+			getDataFolder().mkdir();
+
+		File file = new File(getDataFolder(), "config.yml");
+
+		if (!file.exists())
+		{
+			try (InputStream in = getResourceAsStream("config.yml"))
+			{
+				Files.copy(in, file.toPath());
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
 		getProxy().getPluginManager().registerListener(this, this);
 		getLogger().info("§8[§7UUIDSpoof - Fix§8] §aHas been enabled!");
 	}
@@ -29,20 +62,46 @@ public class FixBungee extends Plugin implements Listener
 	{
 		getLogger().info("§8[§7UUIDSpoof - Fix§8] §cHas been disabled!");
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onJoin(PostLoginEvent event)
 	{
 		ProxiedPlayer player = (ProxiedPlayer) event.getPlayer();
-		String uuid = player.getUniqueId().toString();
-		
-		if(UUIDFetcher.get(player.getName().toString()).contains(uuid))
-		{	
-		}
-		else
+		UUID uuidFetch = UUID.nameUUIDFromBytes(("OfflinePlayer:" + player.getName()).getBytes(Charsets.UTF_8));
+
+		try
 		{
-			TextComponent msg = new TextComponent("§cError!\n§7§nSeems that your UUID is Spoofed! Maybe this is an error, please try to restart your client or change version!");
-			player.disconnect(new BaseComponent[]{ msg });
+			Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+			if (configuration.getBoolean("online-mode") == false)
+			{
+				String uuid = player.getUniqueId().toString();
+				if (uuidFetch.toString().contains(uuid))
+				{
+				}
+				else
+				{
+					TextComponent msg = new TextComponent("§cError!\n§7§nSeems that your UUID is Spoofed! Maybe this is an error, please try to restart your client or change version!");
+					player.disconnect(new BaseComponent[] { msg });
+				}
+				getLogger().info("§6" + player.getName().toString() + "§e's REAL UUID is §6" + uuidFetch.toString());
+			}
+			else
+			{
+				String uuid = player.getUniqueId().toString().replaceAll("-", "");
+				if (UUIDFetcher.get(player.getName().toString()).contains(uuid))
+				{
+				}
+				else
+				{
+					TextComponent msg = new TextComponent("§cError!\n§7§nSeems that your UUID is Spoofed! Maybe this is an error, please try to restart your client or change version!");
+					player.disconnect(new BaseComponent[] { msg });
+				}
+				getLogger().info("§6" + player.getName().toString() + "§e's REAL UUID is §6" + UUIDFetcher.get(player.getName().toString()));
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
 		}
 	}
 }
